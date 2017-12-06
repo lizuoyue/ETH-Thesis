d8aa9fc567aa100fec893d8250c7a673d5fced20
lizuoyue <li.zuoyue@foxmail.com>
2017-12-06 11:45:01 +0100

    a

--- PlotLoss.py
+++ PlotLoss.py
@@ -2,30 +2,34 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
 def save(var1, var2, file):
-	plt.plot(var1)
-	plt.plot(var2)
-	plt.savefig(file)
 	plt.gcf().clear()
+	plt.plot(var1[-1000:], 'b')
+	plt.plot(var2[-1000:], 'r')
+	plt.savefig(file)
 	return
 
 def mean(li):
 	return sum(li) / float(len(li))
 
 if __name__ == '__main__':
-	p = 500
+	p = 300
 	f = open('./PolyRNN.out', 'r')
 	lines = f.readlines()
 	f.close()
-	cnn = rnn = full = []
-	cnn_m = rnn_m = full_m = []
-	for i, line in enumerate(lines):
+	cnn = []
+	rnn = []
+	full = []
+	cnn_m = []
+	rnn_m = []
+	full_m = []
+	for line in lines:
 		a, b, c, d = line.strip().split(', ')
 		cnn.append(float(b))
 		rnn.append(float(c))
 		full.append(float(d))
-		cnn_m.append(mean(cnn[max(len(cnn) - 200, 0):]))
-		rnn_m.append(mean(cnn[max(len(rnn) - 200, 0):]))
-		full_m.append(mean(cnn[max(len(full) - 200, 0):]))	
-	save(cnn, cnn_m, './cnn.pdf')
-	save(rnn, rnn_m, './rnn.pdf')
-	save(full, full_m, './full.pdf')
+		cnn_m.append(mean(cnn[max(len(cnn) - p, 0):]))
+		rnn_m.append(mean(rnn[max(len(rnn) - p, 0):]))
+		full_m.append(mean(full[max(len(full) - p, 0):]))	
+	save(cnn, cnn_m, './plot-cnn.png')
+	save(rnn, rnn_m, './plot-rnn.png')
+	save(full, full_m, './plot-full.png')
