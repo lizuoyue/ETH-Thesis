b09098a6155cec642b4442c44650d1140d419e6a
lizuoyue <li.zuoyue@foxmail.com>
2017-12-13 10:38:25 +0100

    a

--- Remove.py
+++ Remove.py
@@ -29,9 +29,12 @@ if __name__ == '__main__':
 	building_id_list = os.popen('ls ../Chicago').read().strip().split('\n')
 	for i, bid in enumerate(building_id_list):
 		print(i, bid)
-		with open('../Chicago/%s/shift.txt' % bid) as f:
-			lines = f.readlines()
-			edge_prob, _ = lines[1].strip().split()
-			edge_prob = float(edge_prob)
-		if edge_prob < 0.9:
+		try:
+			with open('../Chicago/%s/shift.txt' % bid) as f:
+				lines = f.readlines()
+				edge_prob, _ = lines[1].strip().split()
+				edge_prob = float(edge_prob)
+			if edge_prob < 0.9:
+				os.system('rm -r ../Chicago/%s' % bid)
+		except:
 			os.system('rm -r ../Chicago/%s' % bid)
