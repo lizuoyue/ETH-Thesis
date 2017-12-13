17286f63e15c8f07fa72777cc2f2cce303a8d9dc
lizuoyue <li.zuoyue@foxmail.com>
2017-12-13 01:01:50 +0100

    a

---
+++ Remove.py
@@ -0,0 +1,27 @@
+import os, zipfile
+
+def removeBuilding(archive, building_id, th = 0.9):
+	path = 'Chicago/' + str(building_id)
+
+	#
+	lines = archive.read(path + '/shift.txt').decode('utf-8').split('\n')
+	edge_prob, _ = lines[1].strip().split()
+	edge_prob = float(edge_prob)
+
+	#
+	if edge_prob < th:
+		os.system('zip -d ../Chicago.zip \"*/%s/*\"' % building_id)
+	return
+
+if __name__ == '__main__':
+	archive = zipfile.ZipFile('../Chicago.zip', 'r')
+	building_id_set = set()
+	for filename in archive.namelist():
+		if filename.startswith('__MACOSX'):
+			continue
+		parts = filename.split('/')
+		if len(parts) == 3:
+			building_id_set.add(int(parts[1]))
+	for bid in building_id_set:
+		removeBuilding(archive, bid)
+			
