c1be02bd8782730324332e5a6577acef15aba598
lizuoyue <li.zuoyue@foxmail.com>
2017-12-13 18:46:32 +0100

    a

--- Remove.py
+++ Remove.py
@@ -1,31 +1,6 @@
-import os, zipfile
-
-def removeBuilding(archive, building_id, th = 0.9):
-	path = 'Chicago/' + str(building_id)
-
-	#
-	lines = archive.read(path + '/shift.txt').decode('utf-8').split('\n')
-	edge_prob, _ = lines[1].strip().split()
-	edge_prob = float(edge_prob)
-
-	#
-	if edge_prob < th:
-		os.system('zip -d ../Chicago.zip \"*/%s/*\"' % building_id)
-	return
+import os, sys, zipfile
 
 if __name__ == '__main__':
-	# archive = zipfile.ZipFile('../Chicago.zip', 'r')
-	# building_id_set = set()
-	# for filename in archive.namelist():
-	# 	if filename.startswith('__MACOSX'):
-	# 		continue
-	# 	parts = filename.split('/')
-	# 	if len(parts) == 3:
-	# 		building_id_set.add(int(parts[1]))
-	# for bid in building_id_set:
-	# 	print(bid)
-	# 	removeBuilding(archive, bid)
-
 	building_id_list = os.popen('ls ../Chicago').read().strip().split('\n')
 	for i, bid in enumerate(building_id_list):
 		print(i, bid)
@@ -34,7 +9,7 @@ if __name__ == '__main__':
 				lines = f.readlines()
 				edge_prob, _ = lines[1].strip().split()
 				edge_prob = float(edge_prob)
-			if edge_prob < 0.9:
+			if edge_prob < float(sys.argv[1]):
 				os.system('rm -r ../Chicago/%s' % bid)
 		except:
 			os.system('rm -r ../Chicago/%s' % bid)
