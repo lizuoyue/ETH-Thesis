c632aa635dddd8d8fc025ff52db978dc99a66cc5
lizuoyue <li.zuoyue@foxmail.com>
2017-12-18 10:33:17 +0100

    a

--- RPN/RPN.py
+++ RPN/RPN.py
@@ -7,52 +7,15 @@ import tensorflow as tf
 from PIL import Image, ImageDraw
 ut = __import__('Utility')
 
-class PolygonRNN(object):
+class RPN(object):
 
-	def __init__(self, max_seq_len, lstm_out_channel, v_out_res, train_batch_size, pred_batch_size):
-		# v_out_res: (col, row)
-		assert(v_out_res == (28, 28) or v_out_res == (56, 56))
+	def __init__(self):
+		pass
 
-		# Parameters transferred in
-		self.max_seq_len = max_seq_len
-		self.lstm_out_channel = lstm_out_channel
-		self.v_out_res = v_out_res
-		self.train_batch_size = train_batch_size
-		self.pred_batch_size = pred_batch_size
-
-		# Parameters computed
-		self.res_num = self.v_out_res[1] * self.v_out_res[0]
-		self.lstm_in_channel = [133] + lstm_out_channel[: -1]
-
-		# Create multi-layer LSTM and inital state
-		self.stacked_lstm = tf.contrib.rnn.MultiRNNCell(
-			[self.ConvLSTMCell(in_c, out_c) for in_c, out_c in zip(self.lstm_in_channel, self.lstm_out_channel)]
-		)
-		self.lstm_init_state = []
-		for i, c_out in enumerate(lstm_out_channel):
-			self.lstm_init_state.append(tf.get_variable('conv_lstm_cell_%d_c_h' % i, [2, self.v_out_res[1], self.v_out_res[0], c_out]))
-
-		# Create vertex pool for prediction
-		self.vertex_pool = []
-		for i in range(v_out_res[1]):
-			for j in range(v_out_res[0]):
-				self.vertex_pool.append(np.zeros((v_out_res[1], v_out_res[0]), dtype = np.float32))
-				self.vertex_pool[i * v_out_res[0] + j][i, j] = 1.0
-		self.vertex_pool.append(np.zeros((v_out_res[1], v_out_res[0]), dtype = np.float32))
-		self.vertex_pool = np.array(self.vertex_pool)
-
-	def ConvLSTMCell(self, input_channels, output_channels):
-		return tf.contrib.rnn.ConvLSTMCell(
-			conv_ndims = 2,
-			input_shape = [self.v_out_res[1], self.v_out_res[0], input_channels],
-			output_channels = output_channels,
-			kernel_shape = [3, 3]
-		)
-
-	def ModifiedVGG16For28(self, x, reuse = None):
-		with tf.variable_scope('ModifiedVGG16For28', reuse = reuse):
+	def VGG16(self, img, reuse = None):
+		with tf.variable_scope('VGG16', reuse = reuse):
 			conv1_1 = tf.layers.conv2d(
-				inputs = x,
+				inputs = img,
 				filters = 64,
 				kernel_size = (3, 3),
 				padding = 'same',
@@ -162,187 +125,7 @@ class PolygonRNN(object):
 				padding = 'same',
 				activation = tf.nn.relu
 			)
-			part1_pool = tf.layers.max_pooling2d(
-				inputs = pool2,
-				pool_size = (2, 2),
-				strides = 2
-			)
-			part1 = tf.layers.conv2d(
-				inputs = part1_pool,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part2 = tf.layers.conv2d(
-				inputs = pool3,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part3 = tf.layers.conv2d(
-				inputs = conv4_3,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part4_conv = tf.layers.conv2d(
-				inputs = conv5_3,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part4 = tf.image.resize_images(
-				images = part4_conv,
-				size = [28, 28]
-			)
-			comb = tf.concat([part1, part2, part3, part4], 3)
-			feature = tf.layers.conv2d(
-				inputs = comb,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			return feature
-
-	def ModifiedVGG16For56(self, x, reuse = None):
-		with tf.variable_scope('ModifiedVGG16For56', reuse = reuse):
-			conv1_1 = tf.layers.conv2d(
-				inputs = x,
-				filters = 64,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			conv1_2 = tf.layers.conv2d(
-				inputs = conv1_1,
-				filters = 64,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			pool1 = tf.layers.max_pooling2d(
-				inputs = conv1_2,
-				pool_size = (2, 2),
-				strides = 2
-			)
-			conv2_1 = tf.layers.conv2d(
-				inputs = pool1,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			conv2_2 = tf.layers.conv2d(
-				inputs = conv2_1,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			pool2 = tf.layers.max_pooling2d(
-				inputs = conv2_2,
-				pool_size = (2, 2),
-				strides = 2
-			)
-			conv3_1 = tf.layers.conv2d(
-				inputs = pool2,
-				filters = 256,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			conv3_2 = tf.layers.conv2d(
-				inputs = conv3_1,
-				filters = 256,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			conv3_3 = tf.layers.conv2d(
-				inputs = conv3_2,
-				filters = 256,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			pool3 = tf.layers.max_pooling2d(
-				inputs = conv3_3,
-				pool_size = (2, 2),
-				strides = 2
-			)
-			conv4_1 = tf.layers.conv2d(
-				inputs = pool3,
-				filters = 512,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			conv4_2 = tf.layers.conv2d(
-				inputs = conv4_1,
-				filters = 512,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			conv4_3 = tf.layers.conv2d(
-				inputs = conv4_2,
-				filters = 512,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part1_pool = tf.layers.max_pooling2d(
-				inputs = pool1,
-				pool_size = (2, 2),
-				strides = 2
-			)
-			part1 = tf.layers.conv2d(
-				inputs = part1_pool,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part2 = tf.layers.conv2d(
-				inputs = pool2,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part3 = tf.layers.conv2d(
-				inputs = conv3_3,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part4_conv = tf.layers.conv2d(
-				inputs = conv4_3,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			part4 = tf.image.resize_images(
-				images = part4_conv,
-				size = [56, 56],
-				method = tf.image.ResizeMethod.BICUBIC
-			)
-			comb = tf.concat([part1, part2, part3, part4], 3)
-			feature = tf.layers.conv2d(
-				inputs = comb,
-				filters = 128,
-				kernel_size = (3, 3),
-				padding = 'same',
-				activation = tf.nn.relu
-			)
-			return feature
+			return conv5_3
 
 	def CNN(self, img, boundary_true = None, vertices_true = None, reuse = None):
 		if self.v_out_res == (28, 28):
@@ -384,115 +167,12 @@ class PolygonRNN(object):
 			v_first = tf.gather(self.vertex_pool, idx, axis = 0)
 			return tf.concat([feature, boundary, vertices], 3), v_first
 
-	def RNN(self, feature, v_in = None, rnn_out_true = None, seq_len = None, v_first = None, reuse = None):
-		if not reuse:
-			feature_rep = tf.tile(
-				tf.reshape(feature, [-1, 1, self.v_out_res[1], self.v_out_res[0], 130]),
-				[1, self.max_seq_len, 1, 1, 1]
-			)
-			v_in_0 = tf.tile(v_in[:, 0: 1, ...], [1, self.max_seq_len, 1, 1, 1])
-			v_in_1 = v_in
-			v_in_2 = tf.stack([v_in[:, 0, ...]] + tf.unstack(v_in, axis = 1)[: -1], axis = 1)
-			rnn_input = tf.concat([feature_rep, v_in_0, v_in_1, v_in_2], axis = 4)
-			# v_in_0:   0 0 0 0 0 ... 0
-			# v_in_1:   0 1 2 3 4 ... N - 1
-			# v_in_2:   0 0 1 2 3 ... N - 2
-			# rnn_out:  1 2 3 4 5 ... N
-			# initial_state = self.stacked_lstm.zero_state(self.train_batch_size, tf.float32)
-			initial_state = tuple([tf.contrib.rnn.LSTMStateTuple(
-				c = tf.tile(self.lstm_init_state[i][0: 1], [self.train_batch_size, 1, 1, 1]),
-				h = tf.tile(self.lstm_init_state[i][1: 2], [self.train_batch_size, 1, 1, 1])
-			) for i in range(len(lstm_out_channel))])
-			outputs, state = tf.nn.dynamic_rnn(
-				cell = self.stacked_lstm,
-				inputs = rnn_input,
-				sequence_length = seq_len,
-				initial_state = initial_state,
-				dtype = tf.float32
-			)
-			return self.FC(outputs, rnn_out_true, seq_len)
-		else:
-			v = [None for i in range(self.max_seq_len)]
-			state = [None for i in range(self.max_seq_len)]
-			rnn_output = [None for i in range(self.max_seq_len)]
-			v[0] = tf.reshape(v_first, [-1, self.v_out_res[1], self.v_out_res[0], 1])
-			# state[0] = self.stacked_lstm.zero_state(self.pred_batch_size, tf.float32)
-			state[0] = tuple([tf.contrib.rnn.LSTMStateTuple(
-				c = tf.tile(self.lstm_init_state[i][0: 1], [self.pred_batch_size, 1, 1, 1]),
-				h = tf.tile(self.lstm_init_state[i][1: 2], [self.pred_batch_size, 1, 1, 1])
-			) for i in range(len(lstm_out_channel))])
-			for i in range(1, self.max_seq_len):
-				rnn_output[i], state[i] = self.stacked_lstm(
-					inputs = tf.concat([feature, v[0], v[max(i - 1, 0)], v[max(i - 2, 0)]], 3),
-					state = state[i - 1]
-				)
-				v[i] = tf.reshape(
-					self.FC(
-						rnn_output = rnn_output[i],
-						reuse = True
-					),
-					[-1, self.v_out_res[1], self.v_out_res[0], 1]
-				)
-			return tf.stack(v, 1)
 
-	def FC(self, rnn_output, rnn_out_true = None, seq_len = None, reuse = None):
-		if not reuse:
-			output_reshape = tf.reshape(rnn_output, [-1, self.max_seq_len, self.res_num * self.lstm_out_channel[-1]])
-		else:
-			output_reshape = tf.reshape(rnn_output, [-1, 1, self.res_num * self.lstm_out_channel[-1]])
-		with tf.variable_scope('FC', reuse = reuse):
-			logits = tf.layers.dense(
-				inputs = output_reshape,
-				units = self.res_num + 1,
-				activation = None
-			)
-		if not reuse:
-			loss = tf.reduce_sum(
-				tf.nn.softmax_cross_entropy_with_logits(
-					labels = rnn_out_true,
-					logits = logits
-				)
-			) / tf.reduce_sum(seq_len)
-			return logits, loss
-		else:
-			idx = tf.argmax(logits, axis = 2)
-			return tf.gather(self.vertex_pool, idx, axis = 0)
+	def Train(self):
+		pass
 
-	def Train(self, xx, bb, vv, ii, oo, ee, ll):
-		img           = tf.reshape(xx, [-1, 224, 224, 3])
-		boundary_true = tf.reshape(bb, [-1, self.v_out_res[1], self.v_out_res[0], 1])
-		vertices_true = tf.reshape(vv, [-1, self.v_out_res[1], self.v_out_res[0], 1])
-		v_in          = tf.reshape(ii, [-1, self.max_seq_len, self.v_out_res[1], self.v_out_res[0], 1])
-		v_out_true    = tf.reshape(oo, [-1, self.max_seq_len, self.res_num])
-		end_true      = tf.reshape(ee, [-1, self.max_seq_len, 1])
-		seq_len       = tf.reshape(ll, [-1])
-		rnn_out_true  = tf.concat([v_out_true, end_true], 2)
-
-		feature, loss_CNN = self.CNN(img, boundary_true, vertices_true)
-		logits , loss_RNN = self.RNN(feature, v_in, rnn_out_true, seq_len)
-		boundary = feature[..., -2: -1]
-		vertices = feature[..., -1:]
-
-		# Return
-		rnn_pred = tf.nn.softmax(logits)
-		v_out_pred = tf.reshape(
-			rnn_pred[..., 0: self.res_num],
-			[-1, self.max_seq_len, self.v_out_res[1], self.v_out_res[0]]
-		)
-		end_pred = tf.reshape(
-			rnn_pred[..., self.res_num],
-			[-1, self.max_seq_len, 1]
-		)
-		return loss_CNN, loss_RNN, boundary, vertices, v_out_pred, end_pred
-
-	def Predict(self, xx):
-		img = tf.reshape(xx, [-1, 224, 224, 3])
-		feature, v_first = self.CNN(img, reuse = True)
-		v_out_pred = self.RNN(feature, v_first = v_first, reuse = True)
-		boundary = feature[..., -2: -1]
-		vertices = feature[..., -1:]
-		v_out_pred = v_out_pred[..., 0]
-		return boundary, vertices, v_out_pred
+	def Predict(self):
+		pass
 
 def overlay(img, mask, shape, color = (255, 0, 0)):
 	org = Image.fromarray(np.array(img * 255.0, dtype = np.uint8)).convert('RGBA')
@@ -655,10 +335,10 @@ if __name__ == '__main__':
 	toy = False
 	data_path = '../Chicago.zip'
 	if not toy:
-		lr = 0.00025
+		lr = 0.0005
 		max_seq_len = 24
 		lstm_out_channel = [32, 16, 8]
-		v_out_res = (56, 56) # (28, 28)
+		v_out_res = (28, 28)
 		train_batch_size = 9
 		pred_batch_size = 25
 	else:
