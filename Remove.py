39592450fdc68ebe2837075d7e6440a2e13f1d9d
lizuoyue <li.zuoyue@foxmail.com>
2018-01-12 08:57:53 +0100

    a

--- Remove.py
+++ Remove.py
@@ -1,15 +1,29 @@
-import os, sys, zipfile
+import os, sys
 
 if __name__ == '__main__':
-	building_id_list = os.popen('ls ../Chicago').read().strip().split('\n')
-	for i, bid in enumerate(building_id_list):
-		print(i, bid)
-		try:
-			with open('../Chicago/%s/shift.txt' % bid) as f:
-				lines = f.readlines()
-				edge_prob, _ = lines[1].strip().split()
-				edge_prob = float(edge_prob)
-			if edge_prob < float(sys.argv[1]):
+	if False:
+		building_id_list = os.popen('ls ../Chicago').read().strip().split('\n')
+		for i, bid in enumerate(building_id_list):
+			print(i, bid)
+			try:
+				with open('../Chicago/%s/shift.txt' % bid) as f:
+					lines = f.readlines()
+					edge_prob, _ = lines[1].strip().split()
+					edge_prob = float(edge_prob)
+				if edge_prob < float(sys.argv[1]):
+					os.system('rm -r ../Chicago/%s' % bid)
+			except:
 				os.system('rm -r ../Chicago/%s' % bid)
-		except:
-			os.system('rm -r ../Chicago/%s' % bid)
+	if False:
+		area_id_list = os.popen('ls ../Chicago').read().strip().split('\n')
+		for i, aid in enumerate(area_id_list):
+			print(i, aid)
+			try:
+				with open('../Chicago/%s/shift.txt' % bid) as f:
+					lines = f.readlines()
+					edge_prob, _ = lines[1].strip().split()
+					edge_prob = float(edge_prob)
+				if edge_prob < float(sys.argv[1]):
+					os.system('rm -r ../Chicago/%s' % bid)
+			except:
+				os.system('rm -r ../Chicago/%s' % bid)
--- Utility.py
+++ Utility.py
@@ -1,11 +1,14 @@
-import numpy as np 
-import math, random, time
-import os, io, sys, glob, zipfile
+import numpy as np
+import math, random
+import os, io, sys, glob
+import time, zipfile, paramiko
 from PIL import Image, ImageDraw, ImageFilter
 if os.path.exists('../Python-Lib/'):
 	sys.path.insert(1, '../Python-Lib')
-import paramiko
-# PIL.ImageDraw: 0-based, (col_idx, row_idx) if taking image as matrix
+
+""" Remarks:
+	ImageDraw: 0-based indexing, (col_idx, row_idx) if taking image as matrix
+"""
 
 BLUR = 0.75
 TILE_SIZE = 256
@@ -89,15 +92,295 @@ def plotPolygon(img_size = (224, 224), resolution = (28, 28), num_vertices = 6):
 	# Return
 	return img, boundary, vertices, vertex_list
 
-def intersection(seg1, seg2):
-	assert(seg1[0] <= seg1[1])
-	assert(seg2[0] <= seg2[1])
-	if seg1[1] < seg2[0] or seg2[1] < seg1[0]:
-		return 0
-	else:
-		li = [seg1[0], seg1[1], seg2[0], seg2[1]]
-		li.sort()
-		return li[2] - li[1]
+############################################################
+#  Bounding Boxes
+############################################################
+
+def computeIoU(box, boxes, box_area, boxes_area):
+	"""Calculates IoU of the given box with the array of the given boxes.
+	box: 1D vector [y1, x1, y2, x2]
+	boxes: [boxes_count, (y1, x1, y2, x2)]
+	box_area: float. the area of 'box'
+	boxes_area: array of length boxes_count.
+
+	Note: the areas are passed in rather than calculated here for
+		  efficency. Calculate once in the caller to avoid duplicate work.
+	"""
+	# Calculate intersection areas
+	y1 = np.maximum(box[0], boxes[:, 0])
+	y2 = np.minimum(box[2], boxes[:, 2])
+	x1 = np.maximum(box[1], boxes[:, 1])
+	x2 = np.minimum(box[3], boxes[:, 3])
+	intersection = np.maximum(x2 - x1, 0) * np.maximum(y2 - y1, 0)
+	union = box_area + boxes_area[:] - intersection[:]
+	return intersection / union
+
+def computeOverlaps(boxes1, boxes2):
+	"""Computes IoU overlaps between two sets of boxes.
+	boxes1, boxes2: [N, (y1, x1, y2, x2)].
+
+	For better performance, pass the largest set first and the smaller second.
+	"""
+	# Areas of anchors and GT boxes
+	area1 = (boxes1[:, 2] - boxes1[:, 0]) * (boxes1[:, 3] - boxes1[:, 1])
+	area2 = (boxes2[:, 2] - boxes2[:, 0]) * (boxes2[:, 3] - boxes2[:, 1])
+
+	# Compute overlaps to generate matrix [boxes1 count, boxes2 count]
+	# Each cell contains the IoU value.
+	overlaps = np.zeros((boxes1.shape[0], boxes2.shape[0]))
+	for i in range(overlaps.shape[1]):
+		box2 = boxes2[i]
+		overlaps[:, i] = computeIoU(box2, boxes1, area2[i], area1)
+	return overlaps
+
+def nonMaxSuppression(boxes, scores, threshold):
+	"""Performs non-maximum supression and returns indicies of kept boxes.
+	boxes: [N, (y1, x1, y2, x2)]. Notice that (y2, x2) lays outside the box.
+	scores: 1-D array of box scores.
+	threshold: Float. IoU threshold to use for filtering.
+	"""
+	assert boxes.shape[0] > 0
+	if boxes.dtype.kind != "f":
+		boxes = boxes.astype(np.float32)
+
+	# Compute box areas
+	y1 = boxes[:, 0]
+	x1 = boxes[:, 1]
+	y2 = boxes[:, 2]
+	x2 = boxes[:, 3]
+	area = (y2 - y1) * (x2 - x1)
+
+	# Get indicies of boxes sorted by scores (highest first)
+	ixs = scores.argsort()[::-1]
+
+	pick = []
+	while len(ixs) > 0:
+		# Pick top box and add its index to the list
+		i = ixs[0]
+		pick.append(i)
+		# Compute IoU of the picked box with the rest
+		iou = computeIoU(boxes[i], boxes[ixs[1:]], area[i], area[ixs[1:]])
+		# Identify boxes with IoU over the threshold. This
+		# returns indicies into ixs[1:], so add 1 to get
+		# indicies into ixs.
+		remove_ixs = np.where(iou > threshold)[0] + 1
+		# Remove indicies of the picked and overlapped boxes.
+		ixs = np.delete(ixs, remove_ixs)
+		ixs = np.delete(ixs, 0)
+	return np.array(pick, dtype=np.int32)
+
+def applyBoxesDeltas(boxes, deltas):
+	"""Applies the given deltas to the given boxes.
+	boxes: [N, (y1, x1, y2, x2)]. Note that (y2, x2) is outside the box.
+	deltas: [N, (dy, dx, log(dh), log(dw))]
+	"""
+	boxes = boxes.astype(np.float32)
+	# Convert to y, x, h, w
+	height = boxes[:, 2] - boxes[:, 0]
+	width = boxes[:, 3] - boxes[:, 1]
+	center_y = boxes[:, 0] + 0.5 * height
+	center_x = boxes[:, 1] + 0.5 * width
+	# Apply deltas
+	center_y += deltas[:, 0] * height
+	center_x += deltas[:, 1] * width
+	height *= np.exp(deltas[:, 2])
+	width *= np.exp(deltas[:, 3])
+	# Convert back to y1, x1, y2, x2
+	y1 = center_y - 0.5 * height
+	x1 = center_x - 0.5 * width
+	y2 = y1 + height
+	x2 = x1 + width
+	return np.stack([y1, x1, y2, x2], axis=1)
+
+def boxRefinement(box, gt_box):
+	"""Compute refinement needed to transform box to gt_box.
+	box and gt_box are [N, (y1, x1, y2, x2)]. (y2, x2) is
+	assumed to be outside the box.
+	"""
+	box = box.astype(np.float32)
+	gt_box = gt_box.astype(np.float32)
+
+	height = box[:, 2] - box[:, 0]
+	width = box[:, 3] - box[:, 1]
+	center_y = box[:, 0] + 0.5 * height
+	center_x = box[:, 1] + 0.5 * width
+
+	gt_height = gt_box[:, 2] - gt_box[:, 0]
+	gt_width = gt_box[:, 3] - gt_box[:, 1]
+	gt_center_y = gt_box[:, 0] + 0.5 * gt_height
+	gt_center_x = gt_box[:, 1] + 0.5 * gt_width
+
+	dy = (gt_center_y - center_y) / height
+	dx = (gt_center_x - center_x) / width
+	dh = np.log(gt_height / height)
+	dw = np.log(gt_width / width)
+
+	return np.stack([dy, dx, dh, dw], axis=1)
+
+############################################################
+#  Anchors
+############################################################
+
+def generateAnchors(scales, ratios, shape, feature_stride, anchor_stride):
+	"""
+	scales: 1D array of anchor sizes in pixels. Example: [32, 64, 128]
+	ratios: 1D array of anchor ratios of width/height. Example: [0.5, 1, 2]
+	shape: [height, width] spatial shape of the feature map over which
+			to generate anchors.
+	feature_stride: Stride of the feature map relative to the image in pixels.
+	anchor_stride: Stride of anchors on the feature map. For example, if the
+		value is 2 then generate anchors for every other feature map pixel.
+	"""
+	# Get all combinations of scales and ratios
+	scales, ratios = np.meshgrid(np.array(scales), np.array(ratios))
+	scales = scales.flatten()
+	ratios = ratios.flatten()
+
+	# Enumerate heights and widths from scales and ratios
+	heights = scales / np.sqrt(ratios)
+	widths = scales * np.sqrt(ratios)
+
+	# Enumerate shifts in feature space
+	shifts_y = np.arange(0, shape[0], anchor_stride) * feature_stride
+	shifts_x = np.arange(0, shape[1], anchor_stride) * feature_stride
+	shifts_x, shifts_y = np.meshgrid(shifts_x, shifts_y)
+
+	# Enumerate combinations of shifts, widths, and heights
+	box_widths, box_centers_x = np.meshgrid(widths, shifts_x)
+	box_heights, box_centers_y = np.meshgrid(heights, shifts_y)
+
+	# Reshape to get a list of (y, x) and a list of (h, w)
+	box_centers = np.stack(
+		[box_centers_y + feature_stride/2, box_centers_x+ feature_stride/2], axis=2).reshape([-1, 2])
+	box_sizes = np.stack([box_heights, box_widths], axis=2).reshape([-1, 2])
+
+	# Convert to corner coordinates (y1, x1, y2, x2)
+	boxes = np.concatenate([box_centers - 0.5 * box_sizes,
+							box_centers + 0.5 * box_sizes], axis=1)
+	return boxes
+
+def generatePyramidAnchors(scales, ratios, feature_shapes, feature_strides,
+							 anchor_stride):
+	"""Generate anchors at different levels of a feature pyramid. Each scale
+	is associated with a level of the pyramid, but each ratio is used in
+	all levels of the pyramid.
+
+	Returns:
+	anchors: [N, (y1, x1, y2, x2)]. All generated anchors in one array. Sorted
+		with the same order of the given scales. So, anchors of scale[0] come
+		first, then anchors of scale[1], and so on.
+	"""
+	# Anchors
+	# [anchor_count, (y1, x1, y2, x2)]
+	anchors = []
+	for i in range(len(scales)):
+		anchors.append(generate_anchors(scales[i], ratios, feature_shapes[i],
+										feature_strides[i], anchor_stride))
+	return np.concatenate(anchors, axis=0)
+
+def buildRPNTargets(image_shape, anchors, gt_boxes):
+	"""Given the anchors and GT boxes, compute overlaps and identify positive
+	anchors and deltas to refine them to match their corresponding GT boxes.
+
+	anchors: [num_anchors, (y1, x1, y2, x2)]
+	gt_boxes: [num_gt_boxes, (y1, x1, y2, x2)]
+
+	Returns:
+	rpn_match: [N] (int32) matches between anchors and GT boxes.
+			   1 = positive anchor, -1 = negative anchor, 0 = neutral
+	rpn_bbox: [N, (dy, dx, log(dh), log(dw))] Anchor bbox deltas.
+	"""
+	# RPN Match: 1 = positive anchor, -1 = negative anchor, 0 = neutral
+	rpn_match = np.zeros([anchors.shape[0]], dtype=np.int32)
+	# RPN bounding boxes: [max anchors per image, (dy, dx, log(dh), log(dw))]
+	rpn_bbox = np.zeros((256, 4))
+
+	# Compute overlaps [num_anchors, num_gt_boxes]
+	overlaps = computeOverlaps(anchors, gt_boxes)
+
+	# Match anchors to GT Boxes
+	# If an anchor overlaps a GT box with IoU >= 0.7 then it's positive.
+	# If an anchor overlaps a GT box with IoU < 0.3 then it's negative.
+	# Neutral anchors are those that don't match the conditions above,
+	# and they don't influence the loss function.
+	# However, don't keep any GT box unmatched (rare, but happens). Instead,
+	# match it to the closest anchor (even if its max IoU is < 0.3).
+	#
+	# 1. Set negative anchors first. They get overwritten below if a GT box is
+	# matched to them. Skip boxes in crowd areas.
+	anchor_iou_argmax = np.argmax(overlaps, axis=1)
+	anchor_iou_max = overlaps[np.arange(overlaps.shape[0]), anchor_iou_argmax]
+	rpn_match[(anchor_iou_max < 0.3)] = -1
+	# 2. Set an anchor for each GT box (regardless of IoU value).
+	# TODO: If multiple anchors have the same IoU match all of them
+	gt_iou_argmax = np.argmax(overlaps, axis=0)
+	rpn_match[gt_iou_argmax] = 1
+	# 3. Set anchors with high overlap as positive.
+	rpn_match[anchor_iou_max >= 0.7] = 1
+
+	# Subsample to balance positive and negative anchors
+	# Don't let positives be more than half the anchors
+	ids = np.where(rpn_match == 1)[0]
+	extra = len(ids) - (256 // 2)
+	if extra > 0:
+		# Reset the extra ones to neutral
+		ids = np.random.choice(ids, extra, replace=False)
+		rpn_match[ids] = 0
+	# Same for negative proposals
+	ids = np.where(rpn_match == -1)[0]
+	extra = len(ids) - (256 - np.sum(rpn_match == 1))
+	if extra > 0:
+		# Rest the extra ones to neutral
+		ids = np.random.choice(ids, extra, replace=False)
+		rpn_match[ids] = 0
+
+	# For positive anchors, compute shift and scale needed to transform them
+	# to match the corresponding GT boxes.
+	ids = np.where(rpn_match == 1)[0]
+	ix = 0  # index into rpn_bbox
+	# TODO: use box_refinment() rather than duplicating the code here
+	for i, a in zip(ids, anchors[ids]):
+		# Closest gt box (it might have IoU < 0.7)
+		gt = gt_boxes[anchor_iou_argmax[i]]
+
+		# Convert coordinates to center plus width/height.
+		# GT Box
+		gt_h = gt[2] - gt[0]
+		gt_w = gt[3] - gt[1]
+		gt_center_y = gt[0] + 0.5 * gt_h
+		gt_center_x = gt[1] + 0.5 * gt_w
+		# Anchor
+		a_h = a[2] - a[0]
+		a_w = a[3] - a[1]
+		a_center_y = a[0] + 0.5 * a_h
+		a_center_x = a[1] + 0.5 * a_w
+
+		# Compute the bbox refinement that the RPN should predict.
+		rpn_bbox[ix] = [
+			(gt_center_y - a_center_y) / a_h,
+			(gt_center_x - a_center_x) / a_w,
+			np.log(gt_h / a_h),
+			np.log(gt_w / a_w),
+		]
+
+		# Normalize
+		ix += 1
+
+	return rpn_match, rpn_bbox
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def lurd2xywh(lurd):
 	return (
@@ -114,30 +397,6 @@ def xywh2lurd(xywh):
 		math.floor(xywh[0] + xywh[2] / 2),
 		math.floor(xywh[1] + xywh[3] / 2),
 	)
-
-def scoreIoU(box, anchor):
-	union = box[2] * box[3] + anchor[2] * anchor[3]
-	box = xywh2lurd(box)
-	anchor = xywh2lurd(anchor)
-	inter_x = intersection((anchor[0], anchor[2]), (box[0], box[2]))
-	inter_y = intersection((anchor[1], anchor[3]), (box[1], box[3]))
-	if inter_x > 0 and inter_y > 0:
-		inter = inter_x * inter_y
-		return inter / (union - inter)
-	else:
-		return 0
-
-def findBestBox(bbox, anchor, max_iou = 0.5, min_iou = 0.2):
-	li = [(scoreIoU(box, anchor), i) for i, box in enumerate(bbox)]
-	if not li:
-		return None
-	li.sort()
-	if li[-1][0] >= max_iou:
-		return li[-1][1]
-	elif li[-1][0] < min_iou:
-		return -1
-	else:
-		return None
 
 def plotEllipse(anchor_list, img_size = (640, 640), num_ellipse = 6):
 	# Set image parameters
@@ -553,12 +812,13 @@ class DataGenerator(object):
 
 class AnchorGenerator(object):
 	# num_col, num_row
-	def __init__(self, fake, data_path, anchor_list):
+	def __init__(self, fake, data_path, anchor_para):
 		if fake:
 			self.fake = True
-			self.anchor_list = anchor_list
+			self.anchor_scale, self.anchor_ratio = anchor_para
 		else:
 			self.fake = False
+			self.anchor_scale, self.anchor_ratio = anchor_para
 
 			# 
 			self.ssh = paramiko.SSHClient()
@@ -578,8 +838,9 @@ class AnchorGenerator(object):
 			self.idx_list_valid = self.area_idx_list[split:]
 
 			self.data_path = data_path
-			self.anchor_list = anchor_list
-			return
+
+		self.anchors = generateAnchors(self.anchor_scale, self.anchor_ratio, [40, 40], 16, 1)
+		return
 
 	def getDataBatch(self, batch_size, mode = None):
 		# Fake
@@ -616,7 +877,7 @@ class AnchorGenerator(object):
 		img = img.rotate(n_rotate * 90)
 		img_size = img.size
 		img_size_s = (math.floor(img_size[0] / 16), math.floor(img_size[1] / 16))
-		num_anchors = img_size_s[0] * img_size_s[1] * len(self.anchor_list)
+		num_anchors = self.anchors.shape[0]
 
 		org = np.array(img)[..., 0: 3] / 255.0
 		lines = self.sftp.open(path + '/polygons.txt').read().decode('utf-8').split('\n')
@@ -629,7 +890,7 @@ class AnchorGenerator(object):
 					x, y = line.strip().split()
 					polygons[-1].append((int(x), int(y)))
 
-		bboxes = []
+		gt_boxes = []
 		for polygon in polygons:
 			w, h = org_size
 			p = np.array(polygon, np.int32)
@@ -640,42 +901,48 @@ class AnchorGenerator(object):
 			if r > l and d > u:
 				for _ in range(n_rotate):
 					(w, h), (l, u, r, d) = rotate((w, h), (l, u, r, d))
-				bboxes.append(list(lurd2xywh((l, u, r, d))))
+				gt_boxes.append([u, l, d, r])
 
 		anchor_cls = np.zeros([num_anchors, 2], np.int32)
-		anchor_box = np.zeros([num_anchors, 4], np.float32)
-		pos_anchor = []
-		neg_anchor = []
-		shape = []
-		idx = -1
-		for i in range(img_size_s[1]):
-			for j in range(img_size_s[0]):
-				x = j * 16 + 8
-				y = i * 16 + 8
-				for k, (w, h) in enumerate(self.anchor_list):
-					idx += 1
-					l, u, r, d = xywh2lurd((x, y, w, h))
-					shape.append((l, u, r, d))
-					if l >= 0 and u >= 0 and r < img_size[0] and d < img_size[1]:
-						box_idx = findBestBox(bboxes, (x, y, w, h))
-						if box_idx is not None:
-							if box_idx >= 0:
-								pos_anchor.append(idx)
-								box = bboxes[box_idx]
-								anchor_box[idx, 0] = (box[0] - x) / w
-								anchor_box[idx, 1] = (box[1] - y) / h
-								anchor_box[idx, 2] = math.log(box[2] / w)
-								anchor_box[idx, 3] = math.log(box[3] / h)
-							else:
-								neg_anchor.append(idx)
+		rpn_bbox = np.zeros([num_anchors, 4], np.int32)
+		if len(gt_boxes) > 0:
+			gt_boxes = np.array(gt_boxes)
+			rpn_match, rpn_bbox = buildRPNTargets(org.shape, self.anchors, gt_boxes)
+			anchor_cls[rpn_match == 1, 0] = 1
+			anchor_cls[rpn_match == -1, 1] = 1
+		# anchor_box = np.zeros([num_anchors, 4], np.float32)
+		# pos_anchor = []
+		# neg_anchor = []
+		# shape = []
+		# idx = -1
+		# for i in range(img_size_s[1]):
+		# 	for j in range(img_size_s[0]):
+		# 		x = j * 16 + 8
+		# 		y = i * 16 + 8
+		# 		for k, (w, h) in enumerate(self.anchor_list):
+		# 			idx += 1
+		# 			l, u, r, d = xywh2lurd((x, y, w, h))
+		# 			shape.append((l, u, r, d))
+		# 			if l >= 0 and u >= 0 and r < img_size[0] and d < img_size[1]:
+		# 				box_idx = findBestBox(bboxes, (x, y, w, h))
+		# 				if box_idx is not None:
+		# 					if box_idx >= 0:
+		# 						pos_anchor.append(idx)
+		# 						box = bboxes[box_idx]
+		# 						anchor_box[idx, 0] = (box[0] - x) / w
+		# 						anchor_box[idx, 1] = (box[1] - y) / h
+		# 						anchor_box[idx, 2] = math.log(box[2] / w)
+		# 						anchor_box[idx, 3] = math.log(box[3] / h)
+		# 					else:
+		# 						neg_anchor.append(idx)
 
-		# Random select
-		if len(pos_anchor) > 0:
-			pos = np.random.choice(len(pos_anchor), min(128, len(pos_anchor)), replace = False)
-			anchor_cls[np.array(pos_anchor)[pos], 0] = 1
-		if len(neg_anchor) > 0:
-			neg = np.random.choice(len(neg_anchor), 256 - len(pos_anchor), replace = False)
-			anchor_cls[np.array(neg_anchor)[neg], 1] = 1
+		# # Random select
+		# if len(pos_anchor) > 0:
+		# 	pos = np.random.choice(len(pos_anchor), min(128, len(pos_anchor)), replace = False)
+		# 	anchor_cls[np.array(pos_anchor)[pos], 0] = 1
+		# if len(neg_anchor) > 0:
+		# 	neg = np.random.choice(len(neg_anchor), 256 - len(pos_anchor), replace = False)
+		# 	anchor_cls[np.array(neg_anchor)[neg], 1] = 1
 
 		# Visualization
 		# draw = ImageDraw.Draw(img)
@@ -690,7 +957,7 @@ class AnchorGenerator(object):
 		# 	draw.polygon([(l, u), (r, u), (r, d), (l, d)], outline = (0, 255, 0))
 		# img.show()
 
-		return org, anchor_cls, anchor_box
+		return org, anchor_cls, rpn_bbox
 
 	def getFakeDataBatch(self, batch_size):
 		res = []
@@ -746,10 +1013,6 @@ class AnchorGenerator(object):
 
 if __name__ == '__main__':
 	ANCHOR_SCALE = [60, 120, 180, 240, 300]
-	ANCHOR_SIZE  = [(1, 1), (1.414, 0.707), (0.707, 1.414)]
-	ANCHOR_LIST  = []
-	for scale in ANCHOR_SCALE:
-		for x, y in ANCHOR_SIZE:
-			ANCHOR_LIST.append((int(scale * x), int(scale * y)))
-	ag = AnchorGenerator(fake = False, data_path = '/local/lizuoyue/Chicago_Area', anchor_list = ANCHOR_LIST)
+	ANCHOR_RATIO = [0.5, 1, 2]
+	ag = AnchorGenerator(fake = False, data_path = '/local/lizuoyue/Chicago_Area', anchor_para = (ANCHOR_SCALE, ANCHOR_RATIO))
 	ag.getDataBatch(4, mode = 'train')
