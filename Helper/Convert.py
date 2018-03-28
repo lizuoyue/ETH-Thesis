e7325ec55751e0035e514086e7b31379678c1999
lizuoyue <li.zuoyue@foxmail.com>
2018-03-28 15:20:44 +0200

    a

--- Helper/Convert.py
+++ Helper/Convert.py
@@ -1,7 +1,7 @@
 import h5py
 import numpy as np
 
-res = []
+res1 = []
 data = h5py.File('../../vgg16_weights.h5', 'r')
 for i in range(0, 40):
 	l = 'layer_%d' % i
@@ -13,5 +13,15 @@ for i in range(0, 40):
 				w = np.transpose(np.array(w), (2, 3, 1, 0))
 				b = np.array(b)
 				print(w.shape, b.shape)
-				res.append((w, b))
-np.save('../../VGG16ConvWeights.npy', res)
+				res1.append((w, b))
+np.save('../../VGG16ConvWeights1.npy', res1)
+
+res2 = []
+ls = ['1_1', '1_2', '2_1', '2_2', '3_1', '3_2', '3_3', '4_1', '4_2', '4_3', '5_1', '5_2', '5_3']
+data = np.load('../../vgg16_weights.npz', 'r')
+for i in ls:
+	w = data['conv' + i + '_W']
+	b = data['conv' + i + '_b']
+	print(w.shape, b.shape)
+	res2.append((w, b))
+np.save('../../VGG16ConvWeights2.npy', res2)
