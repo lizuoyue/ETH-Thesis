b07a77f430f4b052e6fc9558d9a01ffc9ea42a9d
lizuoyue <li.zuoyue@foxmail.com>
2018-03-28 13:28:21 +0200

    a

--- Helper/LookColorMean.py
+++ Helper/LookColorMean.py
@@ -5,7 +5,8 @@ from PIL import Image
 def func(img_type, city_name)
 	files = glob.glob('../../%s%s' % (img_type, city_name))
 	res = [[], [], []]
-	for file in files:
+	for n, file in enumerate(files):
+		print(n)
 		img = np.array(Image.open(file), np.int32)
 		for i in range(3):
 			res[i].append(np.mean(img[..., i]))
