0a3cd2e5a6cfec336656f34eaf5f8f2673021bef
lizuoyue <li.zuoyue@foxmail.com>
2018-04-23 19:40:23 +0200

    a

---
+++ Road/Hough.py
@@ -0,0 +1,27 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+
+img = Image.new('P', (256, 256), color = 255)
+draw = ImageDraw.Draw(img)
+draw.line([(-10, 100), (100, -10)], fill = 0, width = 20)
+draw.line([(50, 50), (256, 256)], fill = 0, width = 20)
+draw.line([(150, 150), (300, 0)], fill = 0, width = 20)
+img.show()
+img = np.array(img.resize((32, 32), resample = Image.BICUBIC))
+
+res = np.zeros((361, 361))
+theta = [i * (1.0/360) * math.pi for i in range(361)]
+for i in range(img.shape[0]):
+	for j in range(img.shape[1]):
+		if img[i, j] < 128:
+			for k, th in enumerate(theta):
+				val = (j * math.cos(th) + i * math.sin(th)) * 8
+				if val >= 0 and val <= 360:
+					res[int(val), k] += 1
+res = res / res.max()
+
+plt.imshow(res)
+plt.show()
+
---
+++ Road/Main.py
@@ -0,0 +1,67 @@
+import math, time
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+from BasicModel import *
+import RoadData
+
+def getData(num):
+	aa, bb = [], []
+	for i in range(num):
+		a, b = RoadData.GetData((256, 256), 30, False)
+		aa.append(a)
+		bb.append(b)
+	return np.array(aa), np.array(bb)
+
+img = tf.placeholder(tf.float32, [None, 256, 256, 3])
+gt = tf.placeholder(tf.float32, [None, 32, 32, 3])
+
+loss = Model('train', img, gt)
+pred = Model('test', img)
+
+optimizer = tf.train.AdamOptimizer(learning_rate = 1e-4)
+train = optimizer.minimize(loss)
+saver = tf.train.Saver(max_to_keep = 1)
+init = tf.global_variables_initializer()
+
+# Launch graph
+with tf.Session() as sess:
+	# Create loggers
+	train_loss = open('./LossTrain.out', 'w')
+	valid_loss = open('./LossValid.out', 'w')
+	if not os.path.exists('./Model/'):
+		os.makedirs('./Model/')
+
+	# Restore weights
+	if len(sys.argv) == 2 and sys.argv[1] != None:
+		saver.restore(sess, './Model/%s.ckpt' % sys.argv[1])
+		iter_obj = range(int(sys.argv[1]) + 1, 10000)
+	else:
+		sess.run(init)
+		iter_obj = range(10000)
+
+	# Main loop
+	for i in iter_obj:
+		aa, bb = getData(8)
+		feed_dict = {img: aa - 160, gt: bb}
+
+		# Training and get result
+		init_time = time.time()
+		_, loss_train = sess.run([train, loss], feed_dict)
+		cost_time = time.time() - init_time
+		train_loss.write('Train Iter %d, %.6lf, %.3lfs\n' % (i, loss_train, cost_time))
+		train_loss.flush()
+
+		# Validation
+		if i % 200 == 0:
+			aa, bb = getData(8)
+			feed_dict = {img: aa - 160, gt: bb}
+			init_time = time.time()
+			loss_valid = sess.run(loss, feed_dict)
+			cost_time = time.time() - init_time
+			valid_loss.write('Valid Iter %d, %.6lf, %.3lfs\n' % (i, loss_valid, cost_time))
+			valid_loss.flush()
+			saver.save(sess, './Model/%s.ckpt' % i)
+
+
+
