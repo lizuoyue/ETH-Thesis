dc2cbcad86538d92b45113210ca1c5009d19427d
lizuoyue <li.zuoyue@foxmail.com>
2018-08-28 10:52:29 +0200

    a

--- Road-Edge/RoadData.py
+++ Road-Edge/RoadData.py
@@ -362,6 +362,7 @@ def recoverMultiPath(img, v_in, v_out, th = 0.5):
 	assert(v_in.shape[0] == v_out.shape[0])
 	res = np.zeros((img.shape[0], img.shape[1]))
 	for i in range(v_in.shape[0]):
+		print(v_in.shape)
 		iii = v_in[i]
 		y1, x1 = np.unravel_index(np.argmax(iii), iii.shape)
 		print(v_out[i].shape)
