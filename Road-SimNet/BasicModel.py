fcb498d19079b7d8221972df51a1c6b7ea721065
lizuoyue <li.zuoyue@foxmail.com>
2018-09-09 23:11:41 +0200

    a

--- Road-SimNet/BasicModel.py
+++ Road-SimNet/BasicModel.py
@@ -52,11 +52,11 @@ def VGG19_SIM(scope, img, reuse = None):
 		conv5_1 = tf.layers.conv2d       (inputs = pool4  , filters = 128, kernel_size = 3, padding = 'same', activation = tf.nn.relu, name = 'conv3_1') #  64
 		conv5_2 = tf.layers.conv2d       (inputs = conv5_1, filters = 128, kernel_size = 3, padding = 'same', activation = tf.nn.relu, name = 'conv3_2') #  64
 		conv5_3 = tf.layers.conv2d       (inputs = conv5_2, filters = 128, kernel_size = 3, padding = 'same', activation = tf.nn.relu, name = 'conv3_3') #  64
-		conv5_4 = tf.layers.conv2d       (inputs = conv5_3, filters = 256, kernel_size = 1, padding = 'same', activation = tf.nn.relu, name = 'conv3_4') #  64
+		conv5_4 = tf.layers.conv2d       (inputs = conv5_3, filters = 512, kernel_size = 1, padding = 'same', activation = tf.nn.relu, name = 'conv3_4') #  64
 		pool5   = tf.layers.max_pooling2d(inputs = conv5_4, pool_size = 2, strides = 2)																	 #  32
-		fc0     = tf.reshape(pool5, [-1, 7 * 7 * 256])
-		fc1     = tf.layers.dense(inputs = fc0, units = 1024, activation = tf.nn.relu)
-		fc2     = tf.layers.dense(inputs = fc1, units =  108, activation = tf.nn.relu)
+		fc0     = tf.reshape(pool5, [-1, 7 * 7 * 512])
+		fc1     = tf.layers.dense(inputs = fc0, units = 4096, activation = tf.nn.relu)
+		fc2     = tf.layers.dense(inputs = fc1, units =  512, activation = tf.nn.relu)
 		fc3     = tf.layers.dense(inputs = fc2, units =    2, activation = None)
 		return tf.nn.softmax(fc3)[..., 0]
 
