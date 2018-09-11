edc14a4ee8709e484c941392f26d2b0dc7eee651
lizuoyue <li.zuoyue@foxmail.com>
2018-09-11 16:10:14 +0200

    a

--- Road-SimNet/RoadData.py
+++ Road-SimNet/RoadData.py
@@ -376,7 +376,7 @@ def recoverMultiPath(img, v_in, v_out, peaks_with_score):
 
 if __name__ == '__main__':
 	for _ in range(1):
-		img, boundary, vertices, vertex_inputs, vertex_outputs, seq_lens = getDataBatch(10, mode = 'train', show = False)
+		img, boundary, vertices, vertex_inputs, vertex_outputs, seq_lens = getDataBatch(10, mode = 'train', show = True)
 	# b = getAllTerminal(a[2][0])
 	# print(b.shape)
 	# quit()
