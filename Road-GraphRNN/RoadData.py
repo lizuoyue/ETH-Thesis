57a1d6a8a90bd3a9306647c8e5a9fcb9c327b2dc
lizuoyue <li.zuoyue@foxmail.com>
2018-09-16 21:59:39 +0200

    a

--- Road-GraphRNN/RoadData.py
+++ Road-GraphRNN/RoadData.py
@@ -266,10 +266,10 @@ def getData(img_id, seq_id, show = False):
 	for i in range(len(g.v)):
 		perm = np.random.permutation(len(g.v))
 		path_v = [g.v[perm[j]] for j in range(len(g.v))]
-		vertex_input = [vertex_pool[r][c] for c, r in path_v]
+		vertex_input = [np.array(vertex_pool[r][c], np.float32) / 255.0 for c, r in path_v]
 		vertex_mask = [np.array(blank) / 255.0]
 		for j in range(len(g.v) - 1):
-			vertex_mask.append(np.maximum(vertex_mask[-1], np.array(vertex_input[j]) / 255.0))
+			vertex_mask.append(np.maximum(vertex_mask[-1], np.array(vertex_input[j], np.float32) / 255.0))
 		vertex_output = [nb_maps[perm[j]] for j in range(len(g.v))]
 		while len(vertex_input) < max_seq_len:
 			vertex_input.append(np.array(blank) / 255.0)
