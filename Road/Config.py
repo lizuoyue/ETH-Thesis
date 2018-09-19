8f7d476c96f35117144ed43aa4241b6f53fddcec
lizuoyue <li.zuoyue@foxmail.com>
2018-09-19 11:09:04 +0200

    a

--- Road/Config.py
+++ Road/Config.py
@@ -25,7 +25,7 @@ class Config(object):
 		# Learning parameters
 		self.NUM_ITER = 250001
 		self.MAX_NUM_VERTICES = 10
-		self.LEARNING_RATE = 1e-5
+		self.LEARNING_RATE = 2e-5
 		self.LSTM_OUT_CHANNEL = [64, 32, 16, 8]
 		self.V_OUT_RES = tuple(self.AREA_SIZE_8)
 		self.AREA_TRAIN_BATCH = 4
