b4f75640d396716e9b3e93308c8dab8034662ecc
lizuoyue <li.zuoyue@foxmail.com>
2018-09-21 08:39:51 +0200

    a

--- Road/LookLoss.py
+++ Road/LookLoss.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
 	# quit()
 
 	n_val = 100
-	loss_cnn, loss_rnn = process('LossTrainChicago.out')
-	loss_cnn_val, loss_rnn_val = process('LossValidChicago.out', n_val)
+	loss_cnn, loss_rnn = process('LossTrain.out')
+	loss_cnn_val, loss_rnn_val = process('LossValid.out', n_val)
 	l = len(loss_cnn)
 	l_val = len(loss_cnn_val)
 
