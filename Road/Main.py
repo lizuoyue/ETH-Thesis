74db00c7c1018160179f1068f36eb4cfda330136
lizuoyue <li.zuoyue@foxmail.com>
2018-10-11 15:46:22 +0200

    a

--- Road/Main.py
+++ Road/Main.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
 				files.sort()
 				num, model_path = files[-1]
 				saver.restore(sess, model_path.replace('.meta', ''))
-				iter_obj = range(20)
+				iter_obj = range(10)
 				choose_train = -1
 				choose_valid = -1
 				choose_test = 0
