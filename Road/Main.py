15c99da7a75b27e98ccfa1053959c0bd71918ecf
lizuoyue <li.zuoyue@foxmail.com>
2018-10-11 13:55:03 +0200

    a

--- Road/Main.py
+++ Road/Main.py
@@ -188,7 +188,7 @@ if __name__ == '__main__':
 
 				multi_roads = []
 				prob_res_li = []
-				for terminal in [all_terminal[1]]:
+				for terminal in [all_terminal[0]]:
 					pred_v_out, prob_res = sess.run(pred_path_res, feed_dict = {ff: feature, tt: terminal})
 					multi_roads.append(pred_v_out[0])
 					prob_res_li.append(prob_res)
@@ -198,9 +198,7 @@ if __name__ == '__main__':
 				os.makedirs('./test_res/%d' % i)
 				for j, pathImg in enumerate(pathImgs):
 					savePNG(img[0], pathImg, path + '%d/%d-%d.png' % ((i,) + indices[j]))
-					print(prob_res_li[j].shape)
 					np.save(path + '%d/%d-%d.npy' % ((i,) + indices[j]), prob_res_li[j])
-
 
 			# Save model
 			if i % 10000 == choose_train:
