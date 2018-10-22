49021040cbbfd9e06afa18adc0c88570216c90b3
lizuoyue <li.zuoyue@foxmail.com>
2018-10-22 23:10:43 +0200

    a

--- Data/Config.py
+++ Data/Config.py
@@ -7,6 +7,28 @@ class Config(object):
 		self.OSM_LAT_STEP = 0.01
 		#####################
 		self.CITY_INFO = {
+			'Chicago': {
+				'city_name': 'Chicago',
+				'map_area': [ # (Lat, Lon)
+					(41.9800, -87.7300),
+					(41.7500, -87.7300),
+					(41.7500, -87.5600),
+					(41.8000, -87.6600),
+					(41.9800, -87.6600)
+				],
+				'map_zoom': 18,
+				'map_scale': 2,
+				'map_size': (600, 600), # (Width 1~600, Height 1~600)
+				'b_size': (300, 300),
+				'b_step': (150, 150),
+				'b_x_range': (-2, 2),
+				'b_y_range': (-2, 2),
+				'r_size': (300, 300),
+				'r_step': (150, 150),
+				'r_x_range': (-2, 2),
+				'r_y_range': (-2, 2),
+				'val_test': lambda lon, lat: 0 if lat >= 41.7680 else (1 if lon <= -87.6480 else 2) # 0,1,2 - train,val,test
+			},
 			'Sunnyvale': {
 				'city_name': 'Sunnyvale',
 				'map_area': [ # (Lat, Lon)
@@ -46,14 +68,16 @@ class Config(object):
 				'r_y_range': (-2, 2),
 				'val_test': lambda lon, lat: 0 if lon >= -122.1000 else (1 if lat <= 37.3920 else 2) # 0,1,2 - train,val,test
 			},
-			'Chicago': {
-				'city_name': 'Chicago',
+			'Boston': {
+				'city_name': 'Boston',
 				'map_area': [ # (Lat, Lon)
-					(41.9800, -87.7300),
-					(41.7500, -87.7300),
-					(41.7500, -87.5600),
-					(41.8000, -87.6600),
-					(41.9800, -87.6600)
+					(42.3535, -71.1100),
+					(42.3935, -71.0000),
+					(42.4335, -71.0000),
+					(42.4335, -71.1840),
+					(42.3085, -71.1840),
+					(42.3085, -71.0550),
+					(42.3400, -71.0450)
 				],
 				'map_zoom': 18,
 				'map_scale': 2,
@@ -66,6 +90,6 @@ class Config(object):
 				'r_step': (150, 150),
 				'r_x_range': (-2, 2),
 				'r_y_range': (-2, 2),
-				'val_test': lambda lon, lat: 0 if lat >= 41.7680 else (1 if lon <= -87.6480 else 2) # 0,1,2 - train,val,test
+				'val_test': lambda lon, lat: 0 if lon <= -71.0300 else (1 if lat <= 42.4150 else 2) # 0,1,2 - train,val,test
 			}
 		}
