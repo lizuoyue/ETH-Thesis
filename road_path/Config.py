e782791bca132ca01f6de4c37d8c73a942cf2217
lizuoyue <li.zuoyue@foxmail.com>
2018-10-31 23:01:16 +0100

    a

--- road_path/Config.py
+++ road_path/Config.py
@@ -26,7 +26,7 @@ class Config(object):
 		# Learning parameters
 		self.NUM_ITER = 500001
 		self.MAX_NUM_VERTICES = 10
-		self.LEARNING_RATE = 1e-4
+		self.LEARNING_RATE = 2e-5
 		self.LSTM_OUT_CHANNEL = [64, 32, 16, 8]
 		self.V_OUT_RES = tuple(self.AREA_SIZE_8)
 		self.AREA_TRAIN_BATCH = 4
@@ -37,13 +37,22 @@ class Config(object):
 		self.VALID_NUM_PATH = 16
 
 		self.PATH = {
-			'roadtracer': {
+			'roadtracer-dalabgpu': {
 				'img-train': '/local/home/zoli/data/roadtracer/train_images',
 				'img-val'  : '/local/home/zoli/data/roadtracer/test_images',
 				'img-test' : '/local/home/zoli/data/roadtracer/test_images',
 				'ann-train': '/local/home/zoli/data/roadtracer/roadtracer_train.json',
 				'ann-val'  : '/local/home/zoli/data/roadtracer/roadtracer_test.json',
 				'ann-test' : '/local/home/zoli/data/roadtracer/roadtracer_test.json',
+				'bias'     : [100.28884016, 101.06444319, 89.79566705]
+			},
+			'roadtracer-leonhard': {
+				'img-train': '/cluster/scratch/zoli/roadtracer/train_images',
+				'img-val'  : '/cluster/scratch/zoli/roadtracer/test_images',
+				'img-test' : '/cluster/scratch/zoli/roadtracer/test_images',
+				'ann-train': '/cluster/scratch/zoli/roadtracer/roadtracer_train.json',
+				'ann-val'  : '/cluster/scratch/zoli/roadtracer/roadtracer_test.json',
+				'ann-test' : '/cluster/scratch/zoli/roadtracer/roadtracer_test.json',
 				'bias'     : [100.28884016, 101.06444319, 89.79566705]
 			},
 			'Chicago': {
