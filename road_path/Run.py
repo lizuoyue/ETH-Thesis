1a7614dbd27170415283f4931d617a2a04789f50
lizuoyue <li.zuoyue@foxmail.com>
2018-11-04 11:20:21 +0100

    a

--- road_path/Run.py
+++ road_path/Run.py
@@ -3,8 +3,10 @@ import os, sys, time
 while True:
 	nvidia = os.popen('nvidia-smi').read()
 	li = nvidia.split('\n')
+	res = []
 	for i in range(3, len(li) + 1):
-		print(int(li[-i][5]))
 		if li[-i].startswith('|==='):
 			break
+		res.append(int(li[-i][5]))
+	print(78)
 	quit()
