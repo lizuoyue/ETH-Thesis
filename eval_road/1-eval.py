167610fd6e0530355cf88bb0e2700ddc0c899c9a
lizuoyue <li.zuoyue@foxmail.com>
2018-11-09 10:58:06 +0100

    a

---
+++ eval_road/1-eval.py
@@ -0,0 +1,94 @@
+import json, os, time
+import numpy as np
+
+def l2dist(a, b):
+	diff = np.array(a) - np.array(b)
+	return np.sqrt(np.dot(diff, diff))
+
+search_r = 3
+search_range = []
+for i in range(-search_r, search_r + 1):
+	for j in range(-search_r, search_r + 1):
+		if l2dist((0, 0), (i, j)) <= search_r:
+			search_range.append((i, j))
+
+def near(vd, v):
+	x, y = v
+	for i, j in search_range:
+		if (x + i, y + j) in vd:
+			return vd.pop((x + i, y + j))
+	return None
+
+dts = json.load(open('../../EvalR/predictions_roadtracer-dalabgpu_vgg16_test.json'))
+gts = json.load(open('../../EvalR/roadtracer_test.json'))
+gt_img_by_id = {}
+for gt in gts['images']:
+	gt_img_by_id[gt['id']] = gt
+
+res = {}
+for dt in dts:
+	img_info = gt_img_by_id[dt['image_id']]
+	lurd = img_info['tile_crop_lurd']
+	# if not (lurd[0] % 256 == 0 and lurd[1] % 256 == 0):
+	# 	continue
+	x_bias, y_bias = img_info['tile_bias']
+	city = img_info['tile_file'].split('_')[0]
+	if city not in res:
+		res[city] = {'v': [], 'vd': {}, 'e': []}
+	assert(len(res[city]['v']) >= len(res[city]['vd']))
+	base_idx = len(res[city]['v'])
+	vs = [(item[0] + round(lurd[0] / 256 * 28) + x_bias * 16 * 28, item[1] + round(lurd[1] / 256 * 28) + y_bias * 16 * 28) for item in dt['vertices']]
+	r_idx2a_idx = {}
+	for r_idx, v in enumerate(vs):
+		f = near(res[city]['vd'], v)
+		if f is None:
+			r_idx2a_idx[r_idx] = base_idx
+			res[city]['v'].append(v)
+			res[city]['vd'][v] = (base_idx, 1)
+			base_idx += 1
+		else:
+			fv, fn = f
+			old_v = res[city]['v'][fv]
+			new_v = (round((old_v[0] * fn + v[0]) / (fn + 1)), round((old_v[1] * fn + v[1]) / (fn + 1)))
+			if new_v not in res[city]['vd']:
+				r_idx2a_idx[r_idx] = fv
+				res[city]['v'][fv] = new_v
+				res[city]['vd'][new_v] = (fv, fn + 1)
+			else:
+				tp = res[city]['vd'][new_v]
+				r_idx2a_idx[r_idx] = tp[0]
+				res[city]['vd'][new_v] = (tp[0], tp[1] + fn + 1)
+		assert(len(res[city]['v']) >= len(res[city]['vd']))
+
+	e1 = [[r_idx2a_idx[s], r_idx2a_idx[t], score] for s, t, score in dt['edges']]
+	e2 = [[r_idx2a_idx[t], r_idx2a_idx[s], score] for s, t, score in dt['edges']]
+	res[city]['e'].extend(e1)
+	res[city]['e'].extend(e2)
+
+
+from PIL import Image, ImageDraw
+os.popen('mkdir out_graph')
+time.sleep(1)
+for k in res:
+	print(k)
+	v = res[k]['v']
+	e = res[k]['e']
+	if False:
+		img = Image.new('P', (4096, 4096), color = 0)
+		draw = ImageDraw.Draw(img)
+		for s, t, score in e:
+			if score > 0.75:
+				draw.line([item * (256 / 28) + 4096 for item in v[s] + v[t]], width = 1, fill = 255)
+		img.show()
+		input()
+		continue
+	for th in np.arange(0.5, 1.0, 0.05):
+		with open('./out_graph/%s_%.2lf.out.graph' % (k, th), 'w') as f:
+			for item in v:
+				x, y = round(item[0] * (256 / 28)), round(item[1] * (256 / 28))
+				f.write('%d %d\n' % (x, y))
+			f.write('\n')
+			for s, t, score in e:
+				if s != t and score >= th:
+					f.write('%d %d\n' % (s, t))
+
---
+++ eval_road/3-uf.py
@@ -0,0 +1,148 @@
+import numpy as np
+import os, glob, random
+from PIL import Image, ImageDraw
+
+class UnionFind:
+	"""Weighted quick-union with path compression.
+	The original Java implementation is introduced at
+	https://www.cs.princeton.edu/~rs/AlgsDS07/01UnionFind.pdf
+	>>> uf = UnionFind(10)
+	>>> for (p, q) in [(3, 4), (4, 9), (8, 0), (2, 3), (5, 6), (5, 9),
+	...                (7, 3), (4, 8), (6, 1)]:
+	...  uf.union(p, q)
+	>>> uf._id
+	[8, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	>>> uf.find(0, 1)
+	True
+	>>> uf._id
+	[3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	"""
+
+	def __init__(self, n):
+		self._id = list(range(n))
+		self._sz = [1] * n
+
+	def _root(self, i):
+		j = i
+		while (j != self._id[j]):
+			self._id[j] = self._id[self._id[j]]
+			j = self._id[j]
+		return j
+
+	def find(self, p, q):
+		return self._root(p) == self._root(q)
+	
+	def union(self, p, q):
+		i = self._root(p)
+		j = self._root(q)
+		if i == j:
+			return
+		if (self._sz[i] < self._sz[j]):
+			self._id[i] = j
+			self._sz[j] += self._sz[i]
+		else:
+			self._id[j] = i
+			self._sz[i] += self._sz[j]
+
+cities = [
+	'boston', 'chicago', 'amsterdam', 'denver',
+	'la', 'montreal', 'paris', 'pittsburgh',
+	'saltlakecity', 'tokyo', 'toronto', 'vancouver',
+	'new york', 'kansas city', 'san diego'
+]
+
+def read_graph(filename):
+	with open(filename) as f:
+		lines = f.readlines()
+	for i, line in enumerate(lines):
+		if line == '\n':
+			sep = i
+			break
+	v = lines[:sep]
+	e = lines[sep+1:]
+	v = [tuple(int(item) for item in line.strip().split()) for line in v]
+	e = [tuple(int(item) for item in line.strip().split()) for line in e]
+	return v, e
+
+def save_graph(filename, v, e):
+	with open(filename, 'w') as f:
+		for vv in v:
+			f.write('%d %d\n' % vv)
+		f.write('\n')
+		for ee in e:
+			f.write('%d %d\n' % ee)
+	return
+
+def l2dist(a, b):
+	diff = np.array(a) - np.array(b)
+	return np.sqrt(np.dot(diff, diff))
+
+
+def min_dist(v, vlist1, vlist2):
+	res = []
+	for _ in range(15):
+		v2 = random.choice(vlist2)
+		v1 = min([(l2dist(v[v1], v[v2]), v1) for v1 in vlist1])[1]
+		v2 = min([(l2dist(v[v1], v[v2]), v2) for v2 in vlist2])[1]
+		res.append((l2dist(v[v1], v[v2]), (v1, v2)))
+	return min(res)
+
+if __name__ == '__main__':
+	os.popen('mkdir out_graph_uf')
+	files = glob.glob('out_graph_clean/*0.55*')# + glob.glob('out_graph_clean/*0.60*')
+	for file in files:
+		print(file)
+		v, e = read_graph(file)
+
+		uf = UnionFind(len(v))
+		for s, t in e:
+			uf.union(s, t)
+
+		dv = {uf_id: set() for uf_id in set(uf._id)}
+		de = {uf_id: [] for uf_id in set(uf._id)}
+		for s, t in e:
+			assert(uf.find(s, t))
+			de[uf._root(s)].append((s, t))
+			dv[uf._root(s)].add(s)
+			dv[uf._root(s)].add(t)
+
+		uf_id_to_merge = [(len(dv[uf_id]), uf_id) for uf_id in dv]
+		uf_id_to_merge.sort(reverse = True)
+		max_uf_size = uf_id_to_merge[0][0]
+		uf_id_to_merge = [(uf_size / max_uf_size * 1000, uf_size, uf_id) for uf_size, uf_id in uf_id_to_merge]
+		uf_id_to_merge = [uf_id for uf_size, true_uf_size, uf_id in uf_id_to_merge if uf_size >= 5 and true_uf_size >= 5]
+		master_uf_id = uf_id_to_merge[0]
+		uf_id_to_merge = uf_id_to_merge[1:]
+
+		while len(uf_id_to_merge) > 0:
+			res = min([min_dist(v, list(dv[master_uf_id]), list(dv[uf_id])) + (uf_id, ) for uf_id in uf_id_to_merge])
+			print(len(uf_id_to_merge), res)
+			if res[0] > 128:
+				break
+			dv[master_uf_id].update(dv[res[2]])
+			de[master_uf_id].extend(list(de[res[2]]))
+			de[master_uf_id].append(res[1])
+			uf_id_to_merge.remove(res[2])
+			dv.pop(res[2])
+			de.pop(res[2])
+
+		e_rec = list(de[master_uf_id])
+		for uf_id in uf_id_to_merge:
+			e_rec.extend(de[uf_id])
+		e_rec = [(v[s], v[t]) for s, t in e_rec]
+		v_rec = set()
+		for s, t in e_rec:
+			v_rec.add(s)
+			v_rec.add(t)
+		v_val2idx = {}
+		new_v = list(v_rec)
+		for k, val in enumerate(new_v):
+			v_val2idx[val] = k
+		new_e = [(v_val2idx[s], v_val2idx[t]) for s, t in e_rec]
+
+		save_graph(file.replace('out_graph_clean', 'out_graph_uf'), new_v, new_e)
+
+
+
+
+
